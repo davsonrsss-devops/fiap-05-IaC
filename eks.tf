4c36bddacbb67ec265a9aca119606e858e52a3c5
Davson Silva <davsonrsss@gmail.com>
2026-07-27 19:50:13 -0300

    Update EKS node group configuration to increase desired and maximum size

--- eks.tf
+++ eks.tf
@@ -14,9 +14,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     general = {
-      desired_size = 2
+      desired_size = 4
       min_size     = 1
-      max_size     = 4
+      max_size     = 6
 
       instance_types = ["t3.medium"]
       capacity_type  = "ON_DEMAND"
